b6b2bfb94368878903d16545e439d51b4039f36b
NozakiManato <yeqiaidou07@gmail.com>
2025-06-22 10:55:45 +0900

    localabo/displayページをログインしていなくも見れるように変更した

--- src/app/(display)/localabo/display/page.tsx
+++ src/app/(display)/localabo/display/page.tsx
@@ -1,14 +1,6 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import { AllAttendanceList } from "@/components/attendance/all-attendance-list";
 
 export default async function DisplayPage() {
-  const { userId } = await auth();
-
-  if (!userId) {
-    redirect("/sign-in");
-  }
-
   return (
     <div className="min-h-screen bg-background p-4">
       <div className="h-full">
