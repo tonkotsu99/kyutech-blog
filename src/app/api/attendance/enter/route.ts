a087da87de58456ec9d81de2823c65a9fa2deed3
NozakiManato <yeqiaidou07@gmail.com>
2025-06-30 16:02:49 +0900

    enterとexitのapiの内容を変更した

--- src/app/api/attendance/enter/route.ts
+++ src/app/api/attendance/enter/route.ts
@@ -1,27 +1,31 @@
-import { toggleCheckedInStatus } from "@/lib/actions";
+import { createAttendance } from "@/lib/prisma/attendance";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
-    const { userId, comment} = body;
+    const { userId } = body;
 
     if (!userId) {
-      return NextResponse.json({ error: "ユーザーIDが必要です" }, { status: 400 });
+      return NextResponse.json(
+        { error: "ユーザーIDが必要です" },
+        { status: 400 }
+      );
     }
 
-    const result = await toggleCheckedInStatus(userId, true, comment)
+    const result = await createAttendance(userId);
 
     return NextResponse.json({
       success: true,
       isCheckedIn: result,
-      message: "チェックインが完了しました"
-    })
+      message: "チェックインが完了しました",
+    });
   } catch (error) {
     console.error("チェックインAPIエラー:", error);
-    
-    const errorMessage = error instanceof Error ? error.message : "チェックインに失敗しました";
 
-    return NextResponse.json({error: errorMessage}, {status: 500})
+    const errorMessage =
+      error instanceof Error ? error.message : "チェックインに失敗しました";
+
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
--- src/app/api/attendance/exit/route.ts
+++ src/app/api/attendance/exit/route.ts
@@ -1,28 +1,30 @@
-import { toggleCheckedInStatus } from "@/lib/actions";
+import { updateAttendance } from "@/lib/prisma/attendance";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
-    const { userId, comment} = body;
+    const { userId } = body;
 
     if (!userId) {
-      return NextResponse.json({ error: "ユーザーIDが必要です。"}, {status: 400})
+      return NextResponse.json(
+        { error: "ユーザーIDが必要です。" },
+        { status: 400 }
+      );
     }
 
-    const result = await toggleCheckedInStatus(userId, false, comment);
+    const result = await updateAttendance(userId);
     return NextResponse.json({
       success: true,
       isCheckedIn: result,
-      message: "チェックアウトが完了しました"
-    })
-
+      message: "チェックアウトが完了しました",
+    });
   } catch (error) {
     console.error("チェックアウトAPIエラー:", error);
 
-    const errorMessage = error instanceof Error ? error.message : "チェックアウトに失敗しました"
+    const errorMessage =
+      error instanceof Error ? error.message : "チェックアウトに失敗しました";
 
-    return NextResponse.json({error: errorMessage}, {status: 500})
-    
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
